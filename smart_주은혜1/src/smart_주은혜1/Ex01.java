c9417838e15b313ff89dffc22373f49769abecc1
vip04 <vip04@DESKTOP-PVBN19R>
2021-05-06 15:53:16 +0900

    20210506_1553

---
+++ smart_주은혜1/src/smart_주은혜1/Ex01.java
@@ -0,0 +1,12 @@
+package smart_ÁÖÀºÇı1;
+
+public class Ex01 {
+	public void gugu() {
+		for(int gop = 2; gop <=9; gop++) {
+			for(int dan = 1; dan <=9; dan ++) {
+				System.out.println(gop + "*" + dan + "=" + gop * dan);
+			}
+		}
+	}
+
+}
---
+++ smart_주은혜1/src/smart_주은혜1/Ex02Test.java
@@ -0,0 +1,11 @@
+package smart_ÁÖÀºÇı1;
+
+public class Ex02Test {
+
+	public static void main(String[] args) {
+		Ex01 ex = new Ex01();
+		ex.gugu();
+
+	}
+
+}
